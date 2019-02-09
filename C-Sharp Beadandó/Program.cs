6a011407aacfa6dbd9bb79c418d1f71e7e85cc38
Horváth Dániel <admin@hdani1337.hu>
2019-02-09 13:12:16 +0100

    Sztori újragondolva, alapok, csata, statisztika

--- C-Sharp Beadandó/Program.cs
+++ C-Sharp Beadandó/Program.cs
@@ -8,10 +8,12 @@ namespace C_Sharp_Beadandó
 {
     class Program
     {
-        public static byte gold = 10;
-        public static int progress = 0;
+        public static byte gold = 0;
+        public static byte attack = 10;
+        public static byte hungryness = 0;
         public static Boolean dead = false;
         public static string playerName;
+        public static string place;
         
 
         public Program() {
@@ -25,31 +27,14 @@ namespace C_Sharp_Beadandó
             string valasz = Console.ReadLine();
             if (valasz.Equals("Igen"))
             {
-                progress = 1;
-                Console.WriteLine("Remek, kezdjünk is bele!\n");
-                Console.WriteLine("2009 januárjában járunk. Az egész város alszik még. Reggel ötkor megcsörren az ébresztőórád, indulnod kell dolgozni. \n" +
-                    "Egy kicsit morcosan készülődsz, hiszen alig látsz ki a fejedből a fáradtság miatt. \nPár óra alvás még jól jött volna, nem igaz? \n" +
-                    "A válasz egyértelmű, de már kár ezzel foglalkozni, lassan elkellene indulni a vasútállomásra. \n" +
-                    "Kiérsz az állomásra, és alig tudsz mozdulni, annyira sok ember van. Mindenki ugyanúgy alszik még, mint ahogyan te magad is. \n" +
-                    "Ez teljesen érthető, mivel éppen még csak most zajlottak le az ünnepek, mindenki otthon pihenne még a családjával. \n" +
-                    "Nem lehet mit tenni ellene, vissza kell rázódni a megszokott kerékvágásba.\nMiközben ezen elmélkedsz, elkezd szállingózni a hó\n" +
-                    "Ennek láttán picit elnosztalgiázol, felidézed a gyerekkorodat. Milyen jó is volt a karácsony gyerekként!\n" +
-                    "De gondolataid hamar elszállnak, ugyanis megérkezik a vonat.\nTe vagy az utolsó, előtted már mindenki felszállt, mégis találsz egy szabad fülkét.\n" +
-                    "Gyorsan helyet foglalsz, és el is indul a vonat. Két megállóval odébb felszáll egy bőrkabátos alak, aki veled szembe ül le.\n" +
-                    "Gyanúsan néz rád, minthogyha ismerne valahonnan. Kellemetlenül érzed magad, de lasan megérkezel a munkahelyedre.\n" +
-                    "Fel is pattansz, de mielőtt leszállnál, a férfi egy cetlit nyom a kezedbe, és közli,\nhogyha bárkinek beszélsz erről, akkor nagy bajba kerülhetsz.\n" +
-                    "Egy telefonszám áll rajta. Amint beérsz a munkahelyedre, abban a pillanatban felhívod a számot. Előfizető nem kapcsolható, de fura érzés fog el.\n" +
-                    "Nagyon lassan telik az idő aznap, de mégis végzel a munkával.\nAz utad természetesen ismét a vasút felé tartana, de ekkor kapsz egy SMS-t egy ismeretlen számról: \n,," + playerName +
-                    ", te vagy a kiválasztott!''" +
-                    "\nValahonnan mégis ismerős a telefonszám. Hiszen ez az, amit reggel az ismeretlen férfi a kezedbe nyomott, szóval megint felhívod!\n" +
-                    "Ismét. Ezen a számon előfizető nem kapcsolható. Már hagynád is az egészet a francba, mire még egy SMS-t kapsz:\n" +
-                    ",,Ne próbálkozz annyit, majd mindennek eljön az ideje!''\nPróbálsz lenyugodni, de egyszerűen nem megy. Beáll a vonat, felszállsz és hazamész.\n" +
-                    "Ezúttal semmi szokatlan. Leagalábbis csak azt hiszed...\n" +
-                    "Belépsz az ajtódon, levetkőzöl, eszel egy kicsit a tegnapi rántotthúsból. Ezután megkeresed a reggeli újságot. Pár perc keresés után megtalálod a szekrényben.\n" +
-                    "Nagyon szokatlan. Sose rakod a szekrénybe az újságot, de nem is törődsz vele.\nKiveszed, alatta egy cetli, amin egy GPS-koordináta van.\n" +
-                    "Éppen hogy csak megnézed, kapsz egy telefonhívást. Édesapád az. Azt mondja, hogy a húgod minden nyom nélkül eltűnt.\n" +
-                    "Rákeresel erre a helyre. Körülbelül 100 kilométerre van tőled, egy barlangot jelöl. A gyanúval élve el is indulsz oda. Itt kezdődik a kalandunk!");
-                barlang();
+                place = "A Sziget";
+                Console.WriteLine("Remek, kezdjünk is bele! (A ,,Help'' szóval bármikor kérhetsz segítséget)\n");
+                Console.WriteLine("Olyan furcsán érzed magad. Kinyitod a szemed, s egy szigeten találod magad furcsa öltözetben.\nKörülnézel, " +
+                    "s támadt pár gondolatod.\nBalra melletted vannak ágak és zsinegek. " +
+                    "Készíthetsz belőle fáklyát.\nElőtted van egy sötét barlang." +
+                    "\nJobbra pedig pár madár keres valami eledelt a homokban." +
+                    "\nMerre mész tovább? (Balra/Előre/Jobbra)");
+                sziget();
             }
 
             else if (valasz.Equals("Nem"))
@@ -59,43 +44,289 @@ namespace C_Sharp_Beadandó
                 Console.ReadKey();
             }
 
+            else if (valasz.Equals("Help")) {
+                help();
+            }
+
+            if (valasz.Equals("Feladom"))
+            {
+                dead = true;
+            }
+
             else
             {
                 wrongAnswer();
+                introduction();
+            }
+        }
+
+        public void sziget() {
+            string valasz = Console.ReadLine();
+
+            if (valasz.Equals("Balra")) {
+                place = "Fáklyakészítő hely";
+                Console.WriteLine("Elkészítetted a fáklyát!");
+                hungryness++;
+                Console.WriteLine("A munka miatt az éhséged " + hungryness + "-re nőtt. Vigyázz, ha az éhséged eléri a 10-es szintet, éhenhalsz!\n" +
+                    "Balra nem tudsz menni, mert arra a tenger van. Jobbra a barlang található, előre pedig magas sziklák.\n" +
+                    "Merre tovább? (Balra/Előre/Jobbra)");
+                sziget2();
+            }
+
+            if (valasz.Equals("Jobbra")) {
+                Console.WriteLine("Elindulsz a madarak felé. Észrevesznek, s nagyon mérgesek lesznek.\nMivel az öltözetednek gyenge a védőereje, ezért megölnek, s felfalják a holttested.");
+                dead = true;
+            }
+
+            if (valasz.Equals("Előre"))
+            {
+                Console.WriteLine("Mivel nincs fáklyád, ezért nem mehetsz be a barlangba! Szóval merre mész?");
+                sziget();
+            }
+
+            if (valasz.Equals("Statisztika")) {
+                stats();
+                sziget();
+            }
+
+            if (valasz.Equals("Feladom"))
+            {
+                dead = true;
+            }
+
+            if (valasz.Equals("Help"))
+            {
+                help();
+                sziget();
+            }
+
+            if (!valasz.Equals("Balra") && !valasz.Equals("Jobbra") && !valasz.Equals("Előre"))
+            {
+                wrongAnswer();
+                sziget();
+            }
+        }
+
+        public void sziget2() {
+            string valasz = Console.ReadLine();
+
+            if (valasz.Equals("Balra"))
+            {
+                Console.WriteLine("Mégis bemész a vízbe, ám olyan mély, hogy azonnal lesüllyedsz az aljára és megfulladsz.");
+                dead = true;
+            }
+
+            if (valasz.Equals("Jobbra"))
+            {
+                place = "A barlang";
+                Console.WriteLine("Beértél a barlangba és meggyújtod a fákját. Azonnal kettéoszlik a barlang.\n" +
+                    "Balról kutyaugatást hallasz, jobbról pedig gyenge fény pislákol. Merre mész? (Balra/Jobbra)");
+                barlang();
+            }
+
+            if (valasz.Equals("Előre"))
+            {
+                Console.WriteLine("Mivel nem tudsz falat mászni, ezért nem mehetsz fel a sziklára! Szóval merre mész?");
+                sziget2();
+            }
+
+            if (valasz.Equals("Statisztika"))
+            {
+                stats();
+                sziget2();
+            }
+
+            if (valasz.Equals("Help"))
+            {
+                help();
+                sziget2();
+            }
+
+            if (valasz.Equals("Feladom"))
+            {
+                dead = true;
+            }
+
+            if (!valasz.Equals("Balra") && !valasz.Equals("Jobbra") && !valasz.Equals("Előre"))
+            {
+                wrongAnswer();
+                sziget2();
             }
         }
 
         public void barlang() {
-            progress = 2;
-            Console.WriteLine("Megérkezel erre a furcsa helyre. Indulnál is felfedezni a barlang rejtelmeit, azonban két ismeretlen alak állja az utad.\n" +
-                "Fegyverrel kényszerítenek, hogy vetkőzz le teljesen és minden tárgyadat dobd el magadtól. A félelem miatt ezt meg is teszed.\n" +
-                "Ezután adnak neked egy rongyos szövetruhát, egy kopott kabátot, 10 aranyat és egy fáklyát. Sok sikert kívánnak neked.\n" +
-                "A sokk és az adrenalin hatására indulsz is a barlangba. Néhány méter séta után máris egy útvesztőbe botlasz.\n" +
-                "Eldöntheted, hogy balra, vagy jobbra folytatod az utat. (Ne a politikára gondolj most)\n" +
-                "Balról medvék morajlását hallod, jobbról pedig gyenge fény pislákol.\n" +
-                "Merre szeretnél menni? (Balra/Jobbra)");
             string valasz = Console.ReadLine();
 
-            if (valasz.Equals("Balra")) {
+            if (valasz.Equals("Balra"))
+            {
+                place = "A farkas";
+                Console.WriteLine("Erőt veszel magadon, és elindulsz az ugatás felé." +
+                    "\nMeglepő, egy farkaskutya az, akinek beszorult a lába egy kő alá. Segítesz neki? (Igen/Nem)");
+                farkas();
             }
 
-            if (valasz.Equals("Jobbra")) {
+            if (valasz.Equals("Jobbra"))
+            {
+                place = "Az erdő";
+                Console.WriteLine("Elindulsz a fény felé, ami egyre erősödik, s egy erdőbe érkezel.");
+                erdo();
             }
 
+            if (valasz.Equals("Statisztika"))
+            {
+                stats();
+                barlang();
+            }
+
+            if (valasz.Equals("Help"))
+            {
+                help();
+                barlang();
+            }
+
+            if (valasz.Equals("Feladom"))
+            {
+                dead = true;
+            }
+
+            if (!valasz.Equals("Balra") && !valasz.Equals("Jobbra"))
+            {
+                wrongAnswer();
+                barlang();
+            }
+        }
+
+        public void farkas() {
+            string valasz = Console.ReadLine();
+
+            if (valasz.Equals("Igen"))
+            {
+                attack += 10;
+                hungryness += 1;
+                Console.WriteLine("Sikerült kiszabadítanod a farkast, aki azonnal odafut hozzád és örömében megnyalogat.\n" +
+                    "Hálájából veled tart az utadon, így a támadási értéked " + attack + "-ra nőtt, ám az éhséged is " + hungryness + "-es szintre emelkedett!\n" +
+                    "Visszamész az új társaddal. Jobbra vissza kimehetsz a szigetre, balra pedig elindulhatsz a pislákoló fény felé.\nMerre haladsz tovább? (Balra/Jobbra)");
+                newFriend();
+            }
+
+            if (valasz.Equals("Nem"))
+            {
+                Console.WriteLine("Hátat fordítasz a farkasnak, teszel pár lépést és sikerül kiszabadulnia.\n" +
+                    "Mivel nem segítettél neki, bosszúból azonnal megöl.");
+                dead = true;
+            }
+
+            if (valasz.Equals("Statisztika"))
+            {
+                stats();
+                farkas();
+            }
+
+            if (valasz.Equals("Help"))
+            {
+                help();
+                farkas();
+            }
+
+            if (valasz.Equals("Feladom"))
+            {
+                dead = true;
+            }
+
+            if (!valasz.Equals("Igen") && !valasz.Equals("Nem"))
+            {
+                wrongAnswer();
+                farkas();
+            }
+        }
+
+        public void newFriend() {
+            string valasz = Console.ReadLine();
+
+            if (valasz.Equals("Balra"))
+            {
+                byte madarakAttack = 25;
+                place = "A sziget";
+                Console.WriteLine("Vissza kiérsz a szigetre, ám veled szemben áll az összes madár. Támadási erejük " + madarakAttack + ". Megtámadod őket? (Igen/nem)");
+                battle(attack,madarakAttack);
+            }
+
+            if (valasz.Equals("Jobbra"))
+            { 
+                place = "Az erdő";
+                Console.WriteLine("Elindulsz a fény felé, ami egyre erősödik, s egy erdőbe érkezel.");
+                erdo();
+            }
+
+            if (valasz.Equals("Statisztika"))
+            {
+                stats();
+                newFriend();
+            }
+
+            if (valasz.Equals("Help"))
+            {
+                help();
+                newFriend();
+            }
+
+            if (valasz.Equals("Feladom"))
+            {
+                dead = true;
+            }
+
+            if (!valasz.Equals("Balra") && !valasz.Equals("Jobbra"))
+            {
+                wrongAnswer();
+                newFriend();
+            }
+        }
+
+        public void erdo() {
+        }
+
+        public void battle(byte playerAttack, byte enemyAttack) {
+            if (playerAttack >= enemyAttack) {
+                Console.WriteLine("Győztél!");
+            }
+
+            if (playerAttack < enemyAttack) {
+                Console.WriteLine("Az ellenség fölényben volt, így meghaltál.");
+                dead = true;
+            }
+        }
+
+        public void help() {
+            Console.WriteLine("\nEz egy szöveg alapú kalandjáték. Csak a zárojelben megadott válaszleghetőségek elfogadottak.\n" +
+                "A játékban bármikor használhatod a ,,Statisztika'' parancsot, ami leírja a pontos információidat.\n" +
+                "A játékot bármikor befejezheted a ,,Feladom'' szóval.\n" +
+                "Sok sikert a játékhoz!");
+        }
+
+        public void stats() {
+            Console.WriteLine("Játékos neve: " + playerName);
+            Console.WriteLine("Vagyon: " + gold + " arany");
+            Console.WriteLine("Éhség: " + hungryness);
+            Console.WriteLine("Helyszín: " + place);
+            Console.WriteLine("Támadási erő: " + attack);
         }
 
         public void wrongAnswer() {
             Console.WriteLine("\nHibás válasz. Kérlek, add meg újra a választ!");
-            progress = 0;
-            introduction();
         }
 
         static void Main(string[] args)
         {
             Console.Title = "hdani1337-AdventureGame";
+            Console.SetWindowSize(120,40);
             new Program();
+            if (hungryness >= 10) {
+                dead = true;
+            }
+
             if (dead) {
-                Console.WriteLine("Meghaltál, vége a játéknak!");
+                Console.WriteLine("Vége a játéknak!");
+                Console.WriteLine("\nNyomj egy gombot a kilépéshez...");
                 Console.ReadKey();
             }
         }
